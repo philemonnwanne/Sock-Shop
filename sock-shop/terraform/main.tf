98680ff4163dbb7f756f50e2466ed39a0eed0b13
philemonnwanne <philemonnwanne@gmail.com>
2023-03-16 17:57:20 +0100

    added IAM resource block for ALB ingress 🧱

--- sock-shop/terraform/main.tf
+++ sock-shop/terraform/main.tf
@@ -73,3 +73,18 @@ module "eks" {
     Maintainer  = "philemon nwanne"
   }
 }
+
+resource "aws_iam_policy" "worker_policy" {
+  name        = "worker-policy"
+  description = "Worker policy for the ALB Ingress"
+
+  policy = file("iam-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "additional" {
+  for_each = module.eks.eks_managed_node_groups
+
+  policy_arn = aws_iam_policy.worker_policy.arn
+  role       = each.value.iam_role_name
+
+}
