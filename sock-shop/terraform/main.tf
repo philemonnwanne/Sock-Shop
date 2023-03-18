fd68f8c6095302b6f4aae88a539e43c420658fe6
philemonnwanne <philemonnwanne@gmail.com>
2023-03-18 03:46:35 +0100

    modified main.tf ✍🏾

--- sock-shop/terraform/main.tf
+++ sock-shop/terraform/main.tf
@@ -110,7 +110,7 @@ resource "aws_iam_policy" "worker_policy" {
   name        = "worker-policy"
   description = "Worker policy for the ALB Ingress"
 
-  policy = file("${path.module}/json/iam-policy.json")
+  policy = file("./json/iam-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "additional" {
@@ -194,151 +194,179 @@ resource "helm_release" "ingress" {
   }
 }
 
-resource "aws_iam_role" "external_dns" {
-  name  = "${module.eks.cluster_name}-external-dns"
+# resource "aws_iam_role" "external_dns" {
+#   name  = "${module.eks.cluster_name}-external-dns"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "arn:aws:iam::${local.account_id}:oidc-provider/${local.oidc_url}"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "${local.oidc_url}:sub": "system:serviceaccount:kube-system:external-dns"
-        }
-      }
-    }
-  ]
-}
-EOF
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Federated": "arn:aws:iam::${local.account_id}:oidc-provider/${local.oidc_url}"
+#       },
+#       "Action": "sts:AssumeRoleWithWebIdentity",
+#       "Condition": {
+#         "StringEquals": {
+#           "${local.oidc_url}:sub": "system:serviceaccount:kube-system:external-dns"
+#         }
+#       }
+#     }
+#   ]
+# }
+# EOF
 
-}
+# }
 
-resource "aws_iam_role_policy" "external_dns" {
-  name_prefix = "${module.eks.cluster_id}-external-dns"
-  role        = aws_iam_role.external_dns.name
-  policy      = file("${path.module}/json/external-dns-iam-policy.json")
-}
+# resource "aws_iam_role_policy" "external_dns" {
+#   name_prefix = "${module.eks.cluster_id}-external-dns"
+#   role        = aws_iam_role.external_dns.name
+#   policy      = file("${path.module}/json/external-dns-iam-policy.json")
+# }
 
-resource "kubernetes_service_account" "external_dns" {
-  metadata {
-    name      = "external-dns"
-    namespace = "kube-system"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.external_dns.arn
-    }
-  }
-  automount_service_account_token = true
-}
+# resource "kubernetes_service_account" "external_dns" {
+#   metadata {
+#     name      = "external-dns"
+#     namespace = "kube-system"
+#     annotations = {
+#       "eks.amazonaws.com/role-arn" = aws_iam_role.external_dns.arn
+#     }
+#   }
+#   automount_service_account_token = true
+# }
 
-resource "kubernetes_cluster_role" "external_dns" {
-  metadata {
-    name = "external-dns"
-  }
+# resource "kubernetes_cluster_role" "external_dns" {
+#   metadata {
+#     name = "external-dns"
+#   }
 
-  rule {
-    api_groups = [""]
-    resources  = ["services", "pods", "nodes"]
-    verbs      = ["get", "list", "watch"]
-  }
-  rule {
-    api_groups = ["extensions", "networking.k8s.io"]
-    resources  = ["ingresses"]
-    verbs      = ["get", "list", "watch"]
-  }
-  rule {
-    api_groups = ["networking.istio.io"]
-    resources  = ["gateways"]
-    verbs      = ["get", "list", "watch"]
-  }
-}
+#   rule {
+#     api_groups = [""]
+#     resources  = ["services", "pods", "nodes"]
+#     verbs      = ["get", "list", "watch"]
+#   }
+#   rule {
+#     api_groups = ["extensions", "networking.k8s.io"]
+#     resources  = ["ingresses"]
+#     verbs      = ["get", "list", "watch"]
+#   }
+#   rule {
+#     api_groups = ["networking.istio.io"]
+#     resources  = ["gateways"]
+#     verbs      = ["get", "list", "watch"]
+#   }
+# }
 
-resource "kubernetes_cluster_role_binding" "external_dns" {
-  metadata {
-    name = "external-dns"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.external_dns.metadata.0.name
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.external_dns.metadata.0.name
-    namespace = kubernetes_service_account.external_dns.metadata.0.namespace
-  }
-}
+# resource "kubernetes_cluster_role_binding" "external_dns" {
+#   metadata {
+#     name = "external-dns"
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = kubernetes_cluster_role.external_dns.metadata.0.name
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = kubernetes_service_account.external_dns.metadata.0.name
+#     namespace = kubernetes_service_account.external_dns.metadata.0.namespace
+#   }
+# }
 
-resource "helm_release" "external_dns" {
-  name       = "external-dns"
-  namespace  = kubernetes_service_account.external_dns.metadata.0.namespace
-  wait       = true
-  repository = data.helm_repository.bitnami.metadata.0.name
-  chart      = "external-dns"
-  version    = var.external_dns_chart_version
+# resource "helm_release" "external_dns" {
+#   name       = "external-dns"
+#   namespace  = kubernetes_service_account.external_dns.metadata.0.namespace
+#   wait       = true
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "external-dns"
+#   version    = var.external_dns_chart_version
 
-  set {
-    name  = "rbac.create"
-    value = false
-  }
+#   set {
+#     name  = "rbac.create"
+#     value = false
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = false
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = false
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = kubernetes_service_account.external_dns.metadata.0.name
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = kubernetes_service_account.external_dns.metadata.0.name
+#   }
 
-  set {
-    name  = "rbac.pspEnabled"
-    value = false
-  }
+#   set {
+#     name  = "rbac.pspEnabled"
+#     value = false
+#   }
 
-  set {
-    name  = "name"
-    value = "${var.cluster_name}-external-dns"
-  }
+#   set {
+#     name  = "name"
+#     value = "${local.cluster_name}-external-dns"
+#   }
 
-  set {
-    name  = "provider"
-    value = "aws"
-  }
+#   set {
+#     name  = "provider"
+#     value = "aws"
+#   }
 
-  set_string {
-    name  = "policy"
-    value = "sync"
-  }
+#   set_string {
+#     name  = "policy"
+#     value = "sync"
+#   }
 
-  set_string {
-    name  = "logLevel"
-    value = var.external_dns_chart_log_level
-  }
+#   set_string {
+#     name  = "logLevel"
+#     value = var.external_dns_chart_log_level
+#   }
 
-  set {
-    name  = "sources"
-    value = "{ingress,service}"
-  }
+#   set {
+#     name  = "sources"
+#     value = "{ingress,service}"
+#   }
 
-  set {
-    name  = "domainFilters"
-    value = "{${join(",", var.external_dns_domain_filters)}}"
-  }
+#   set {
+#     name  = "domainFilters"
+#     value = "{${join(",", var.external_dns_domain_filters)}}"
+#   }
 
-  set_string {
-    name  = "aws.zoneType"
-    value = var.external_dns_zoneType
-  }
+#   set_string {
+#     name  = "aws.zoneType"
+#     value = var.external_dns_zoneType
+#   }
 
-  set_string {
-    name  = "aws.region"
-    value = var.aws_region
-  }
-}
+#   set_string {
+#     name  = "aws.region"
+#     value = var.aws_region
+#   }
+# }
+
+# module "eks_blueprints_kubernetes_addons" {
+#   source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons"
+
+#   cluster_id                    = eks_cluster_id
+
+#   # EKS Addons
+#   #K8s Add-ons
+#   # enable_aws_load_balancer_controller  = true
+#   enable_external_dns = true
+#   enable_kube_prometheus_stack      = true
+#   # enable_vault = true
+# }
+
+# kube_prometheus_stack_helm_config = {
+#   set = [
+#     {
+#       name  = "kubeProxy.enabled"
+#       value = false
+#     }
+#   ],
+#   set_sensitive = [
+#     {
+#       name  = "grafana.adminPassword"
+#       value = data.aws_secretsmanager_secret_version.admin_password_version.secret_string
+#     }
+#   ]
+# }
