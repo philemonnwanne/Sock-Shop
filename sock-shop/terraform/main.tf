a159b6c30d35894de57c5cae1683a8d1fb1b5c55
philemonnwanne <philemonnwanne@gmail.com>
2023-03-18 04:01:58 +0100

    modified main.tf ✍🏾

--- sock-shop/terraform/main.tf
+++ sock-shop/terraform/main.tf
@@ -110,7 +110,7 @@ resource "aws_iam_policy" "worker_policy" {
   name        = "worker-policy"
   description = "Worker policy for the ALB Ingress"
 
-  policy = file("${path.root}/sock-shop/terraform/json/iam-policy.json")
+  policy = file("~/capstone/sock-shop/terraform/json/iam-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "additional" {
