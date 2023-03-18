266e03b18e44ed3d5cd5fdbc2d7d11310eee4eb2
philemonnwanne <philemonnwanne@gmail.com>
2023-03-18 09:16:26 +0100

    modified config.yml ✍🏾

--- sock-shop/terraform/main.tf
+++ sock-shop/terraform/main.tf
@@ -110,7 +110,7 @@ resource "aws_iam_policy" "worker_policy" {
   name        = "worker-policy"
   description = "Worker policy for the ALB Ingress"
 
-  policy = file("iam-policy.json")
+  policy = file("${path.module}/iam-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "additional" {
